cc81f272cdbb6859cbed8ea667b3c7336da46a92
Unknown <janik.hofmann@games-academy.de>
2019-04-24 16:45:58 +0200

    New TestH Folder + CameraController

---
+++ BODY/Assets/Main/Scripts/Player/CameraControllerH.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX = Input.GetAxis("HorizontalRight");
+        var moveY = Input.GetAxis("HorizontalLeft");
+    }
+}
