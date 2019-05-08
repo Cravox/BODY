007b2ac1e9e9b680bffcf364e7466dab692f724b
Jonas Walter <jonas.walter@games-academy.de>
2019-05-08 13:13:41 +0200

    Created IPlayerLimb Interface

--- BODY/Assets/Main/Scripts/Player/PlayerState.cs
+++ BODY/Assets/Main/Scripts/Player/PlayerState.cs
@@ -2,8 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IPlayerLimb
+{
+    PlayerController playerCont { get; set; }
+    bool fullyCharged { get; set; }
+    int energyCount { get; set; }
+
+    void TierOne();
+    void TierTwo();
+
+    void TierThree();
+
+    void Charge();
+    void Discharge();
+    void Update();
+    void FixedUpdate();
+}
+
 public class PlayerState : MonoBehaviour
 {
+    List<IPlayerLimb> playerLimbs = new List<IPlayerLimb>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,17 @@ public class PlayerState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(IPlayerLimb pl in playerLimbs)
+        {
+            pl.Update();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        foreach (IPlayerLimb pl in playerLimbs)
+        {
+            pl.FixedUpdate();
+        }
     }
 }
