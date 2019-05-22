7a96998d2e2f567962d151dac3ecf6bf83d62cf1
Janik <janik.hofmann@games-academy.de>
2019-05-22 17:15:35 +0200

    Box-Goal Mechanic

---
+++ BODY/Assets/Main/Scripts/Puzzle/BoxPlacer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class BoxPlacer : SerializedMonoBehaviour {
+    [SerializeField, Required]
+    private Transform[] boxTrans = new Transform[4];
+    private int currBoxCount = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.CompareTag("Box")) {
+            col.gameObject.transform.position = boxTrans[currBoxCount].position;
+            col.gameObject.transform.eulerAngles = Vector3.zero;
+            col.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            currBoxCount++;
+        }
+    }
+}
