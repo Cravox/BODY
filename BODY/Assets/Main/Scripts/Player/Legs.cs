6676d3ba724db54caada333941f8541480c0fb8c
Jonas Walter <jonas.walter@games-academy.de>
2019-06-03 15:46:05 +0200

    walljump doo doo

--- BODY/Assets/Main/Scripts/Player/Legs.cs
+++ BODY/Assets/Main/Scripts/Player/Legs.cs
@@ -7,12 +7,25 @@ public class Legs : Limb {
     [TabGroup("Balancing"), Header("Double Jump"), Tooltip("X = Sideways, Y = Up, Z = Forward")]
     public Vector3 dJumpSpeed;
     [TabGroup("Balancing")]
-    public float jumpForwardTime = 0.5f;
+    public float dJumpForwardTime = 0.5f;
+    [TabGroup("Balancing")]
+    public float wJumpDistanceToWall = 1f;
+
+    [TabGroup("Balancing"), Header("Wall Jump"), Tooltip("X = Sideways, Y = Up, Z = Forward")]
+    public Vector3 wJumpSpeed;
+    [TabGroup("Balancing")]
+    public float wJumpForwardTime = 0.5f;
 
     [TabGroup("Debugging")]
     public bool doubleJumping;
     [TabGroup("Debugging")]
     public PlayerForce dJumpForce;
+    [TabGroup("Debugging")]
+    public bool wallJumping;
+    [TabGroup("Debugging")]
+    public PlayerForce wJumpForce;
+    [TabGroup("Debugging")]
+    public bool wallJump;
 
     protected override string limbName => "Legs";
 
@@ -26,7 +39,7 @@ public class Legs : Limb {
 
     public override void TierTwo() {
         if(EnergySystem.chargeState == EnergySystem.ChargeState.NOT_CHARGING) {
-            if (Input.GetButtonDown("Jump") && !playerCont.isGrounded && !doubleJumping) {
+            if (Input.GetButtonDown("Jump") && !playerCont.isGrounded && !doubleJumping && !wallJump) {
                 doubleJumping = true;
                 playerCont.modelAnim.Play("Dash");
 
@@ -36,7 +49,7 @@ public class Legs : Limb {
                 Vector3 dirForceUp = playerCont.transform.up * dJumpSpeed.y;
 
                 playerCont.AddForce(dirForceUp, 0, out dJumpForce, true);
-                playerCont.AddForce(dirForceSide, jumpForwardTime, out dJumpForce, false);
+                playerCont.AddForce(dirForceSide, dJumpForwardTime, out dJumpForce, false);
             }
 
             if (playerCont.isGrounded && doubleJumping && dJumpForce != null) {
@@ -49,15 +62,47 @@ public class Legs : Limb {
     }
 
     public override void TierThree() {
-        // gliding or walljumping
+        // walljump (atm)
+        if (EnergySystem.chargeState == EnergySystem.ChargeState.NOT_CHARGING)
+        {
+            if (Input.GetButtonDown("Jump") && !playerCont.isGrounded && !wallJumping && wallJump)
+            {
+                wallJumping = true;
+                playerCont.modelAnim.Play("Dash");
+
+                playerCont.rigid.velocity = new Vector3(playerCont.rigid.velocity.x, 0, playerCont.rigid.velocity.z);
+
+                Vector3 dirForceSide = playerCont.modelAxis.forward * wJumpSpeed.z + playerCont.transform.right * wJumpSpeed.x;
+                Vector3 dirForceUp = playerCont.transform.up * wJumpSpeed.y;
+
+                playerCont.AddForce(dirForceUp, 0, out wJumpForce, true);
+                playerCont.AddForce(dirForceSide, wJumpForwardTime, out wJumpForce, false);
+            }
+
+            if (playerCont.isGrounded && wallJumping && wJumpForce != null)
+            {
+                wJumpForce.Stop();
+                wallJumping = false;
+            }
+        }
+
+        Debug.DrawRay(transform.position, playerCont.modelAxis.transform.forward * wJumpDistanceToWall, Color.green, 0.1f);
+
+        //playerCont.modelAnim.SetBool("IsDashing", wallJumping);
+
+
     }
 
     protected override void LimbStart() {
-
     }
 
     protected override void LimbUpdate() {
+        bool canWallJump = (EnergyState == Enums.EnergyStates.FULLY_CHARGED);
+        bool ray = Physics.Raycast(transform.position, playerCont.modelAxis.transform.forward, wJumpDistanceToWall, LayerMask.GetMask("wJump"));
 
+        Debug.Log(ray);
+
+        wallJump = (canWallJump && ray);
     }
 
     protected override void OnDischarge() {
