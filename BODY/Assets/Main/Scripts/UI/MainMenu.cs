e78f590160cc849e23402aeac0addd29259085df
Jonas Walter <jonas.walter@games-academy.de>
2019-06-18 15:59:40 +0200

    Main menu and in-game menu prototypes

---
+++ BODY/Assets/Main/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void Button_Start()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    public void Button_ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BODY/Assets/Main/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public CanvasGroup panel;
+    public bool isActive = false;
+    public GameObject firstButton;
+
+    [Range(0, 1)]
+    public float fadeSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        panel.interactable = isActive;
+
+        if (panel.alpha != 1 && isActive ||panel.alpha != 0 && !isActive)
+            panel.alpha = Mathf.MoveTowards(panel.alpha, isActive ? 1 : 0, fadeSpeed);
+
+        Time.timeScale = isActive ? 0 : 1;
+    }
+
+    public void Button_Return()
+    {
+        isActive = false;
+    }
+    public void Button_Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Button_Options
+
+    public void Button_Exit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
