552b3eecc3e641be2eb2eb752f2dfa5b15a87742
Janik <janik.hofmann@games-academy.de>
2019-06-18 14:20:49 +0200

    Select-Button Input + PuzzleObjects reset

---
+++ BODY/Assets/Main/Scripts/Puzzle/PuzzleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class PuzzleManager : SerializedMonoBehaviour {
+    [SerializeField, TabGroup("Balancing")]
+    private Transform[] resettableObjects;
+
+    private Vector3[] startObjectPosition;
+    private Vector3[] startObjectEulerAngles;
+
+    // Start is called before the first frame update
+    void Start() {
+        startObjectPosition = new Vector3[resettableObjects.Length];
+        startObjectEulerAngles = new Vector3[resettableObjects.Length];
+        
+        for (int i = 0; i < resettableObjects.Length; i++) {
+            startObjectPosition[i] = resettableObjects[i].position;
+            startObjectEulerAngles[i] = resettableObjects[i].eulerAngles;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("SelectButton")) {
+            ResetObjects();
+        }
+    }
+
+    void ResetObjects() {
+        for (int i = 0; i < resettableObjects.Length; i++) {
+            resettableObjects[i].position = startObjectPosition[i];
+            resettableObjects[i].eulerAngles = startObjectEulerAngles[i];
+        }
+
+    }
+}
