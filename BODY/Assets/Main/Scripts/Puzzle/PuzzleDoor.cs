edc2a5beb5b22af929b377a4600c89bcdbecd09c
Janik <janik.hofmann@games-academy.de>
2019-07-02 14:10:32 +0200

    Opening Doors

---
+++ BODY/Assets/Main/Scripts/Puzzle/PuzzleDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class PuzzleDoor : MonoBehaviour {
+    [SerializeField, TabGroup("References"), Required]
+    private Animator doorAnim;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider col) {
+        if(col.gameObject.CompareTag("Player")) doorAnim.SetBool("Open", true);
+    }
+
+    private void OnTriggerExit(Collider col) {
+        if (col.gameObject.CompareTag("Player")) doorAnim.SetBool("Open", false);
+    }
+}
