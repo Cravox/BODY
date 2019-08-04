794f0d1e4a3b5dc1d8f95f628545b481eb84decf
Janik <janik.hofmann@gmx.de>
2019-08-04 17:35:48 +0200

    Room three

--- BODY/Assets/Main/Scripts/Puzzle/TriggerButton.cs
+++ BODY/Assets/Main/Scripts/Puzzle/TriggerButton.cs
@@ -2,37 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerButton : TriggerObject
-{
+public class TriggerButton : TriggerObject {
     public Animator buttonAnim;
 
+    [SerializeField]
+    private bool player;
+
+    [SerializeField]
+    private bool carryBox;
+
+    [SerializeField]
+    private bool pushBox;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(buttonAnim)
+    void Update() {
+        if (buttonAnim)
             buttonAnim.SetBool("Triggered", triggered);
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Carry")
-        {
-            triggered = true;
+    private void OnTriggerStay(Collider other) {
+        if (player) {
+            if (other.gameObject.tag == "Player") {
+                triggered = true;
+            }
+        }
+
+        if (carryBox) {
+            if (other.gameObject.tag == "Carry") {
+                triggered = true;
+            }
+        }
+
+        if (pushBox) {
+            if (other.gameObject.tag == "Push") {
+                triggered = true;
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Carry")
-        {
-            if(!triggerOnce)
+    private void OnTriggerExit(Collider other) {
+        if (player) {
+            if (other.gameObject.tag == "Player") {
                 triggered = false;
+            }
+        }
+
+        if (carryBox) {
+            if (other.gameObject.tag == "Carry") {
+                triggered = false;
+            }
+        }
+
+        if (pushBox) {
+            if (other.gameObject.tag == "Push") {
+                triggered = false;
+            }
         }
     }
 }
