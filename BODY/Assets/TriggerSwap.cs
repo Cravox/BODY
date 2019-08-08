6a14084b4df3aa5e618e64c5ba2a9552b50b6997
Janik <janik.hofmann@games-academy.de>
2019-08-08 10:52:11 +0200

    Swapping Barriers

---
+++ BODY/Assets/TriggerSwap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSwap : TriggerContainer {
+    [SerializeField]
+    private Transform playerWall;
+
+    [SerializeField]
+    private Transform cubeWall;
+
+    private bool swapped = false;
+
+    Vector3 playerWallPos;
+    Vector3 cubeWallPos;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        playerWallPos = playerWall.position;
+        cubeWallPos = cubeWall.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (gotActive && !swapped) {
+            playerWall.position = cubeWallPos;
+            cubeWall.position = playerWallPos;
+            swapped = true;
+        }
+    }
+}
